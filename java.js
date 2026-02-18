fe00054d704c68aa00ea720d766b7116672cf0e2
PPetra08 <159010793+PPetra08@users.noreply.github.com>
2026-02-18 12:56:56 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+let sorokSzama = document.getElementById("rows");
+let oszlopokSzama = document.getElementById("columns");
+let borderCollapse = document.getElementById("collapse");
+let borderSzelesseg = document.getElementById("border_width");
+let keretSzin = document.getElementById("border_color");
+let container = document.getElementById("container");
+let pxSzam = document.getElementById("px");
+
+function AddTable(){
+for (let pxSzam=0; index< oszlopokSzama; index ++){
+let ujTabla = document.createElement()
+
+}
+}
